ceac148d63a82d81d3cb3a80c2c7186ef0f33595
Sulthan Khansa <sulthankhansa11@gmail.com>
2026-04-04 09:52:55 +0700

    Final Build: Complete ERP System with RBAC and Profit Analytics

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -129,6 +129,9 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
+            ])
+            ->plugins([
+                \BezhanSalleh\FilamentShield\FilamentShieldPlugin::make(),
             ]);
     }
 }
